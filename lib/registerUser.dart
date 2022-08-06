92cb8752d9d79372fbef55b4a9ea318c008f56dc
Greeshmanth <greeshmanth5e3@gmail.com>
2022-08-06 22:00:33 +0530

    commit1

---
+++ lib/registerUser.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InstituteRegister extends StatefulWidget {
+  InstituteRegister({Key? key}) : super(key: key);
+
+  @override
+  State<InstituteRegister> createState() => _InstituteRegisterState();
+}
+
+class _InstituteRegisterState extends State<InstituteRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
